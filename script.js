7e1c993d9989b4d2890806174edffb1e84f1766a
Vanessa Lane <lane.vanessar@gmail.com>
2022-05-31 00:03:09 -0400

    Updated to include comments and comment out tests

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*Vanessa Lane
+MET CS 601 Web Development
+Assignment 3
+This program prompts the user for integer input, sums the input,
+and then returns the sum. There are other functions for input and printing the 
+result that supplement the main purpose of the program.*/
 //Welcome the user with an alert
 alert("Welcome to this program!");
 
@@ -6,6 +12,7 @@ let user = prompt("Please enter your name.");
 
 /*Displaystheusersnamewithanalert
 a. Suchas“Welcome[name]!”*/
+alert("Welcome," + user);
 
 
 
@@ -32,7 +39,7 @@ while (true) {
         var sum = numberOne + numberTwo;
         alert("Here.");
         return sum;
-        /////printResult(sum);
+        // test printResult(sum);
 
     }
     function input() {
@@ -43,11 +50,11 @@ while (true) {
             let userNumbers = prompt(user + ", " + "Please enter two numbers separated by a comma.");
             let numArray = userNumbers.split(",");
             let userNumbersArr = [...userNumbers];
-            alert(numArray);
+            //Test alert(numArray);
             numberOne = parseInt(numArray[0]);
             numberTwo = parseInt(numArray[1]);
-            alert(numberOne);
-            alert(numberTwo);
+            // test alert(numberOne);
+            // test alert(numberTwo);
             if (isNaN(numberOne) || isNaN(numberTwo)) {
                 alert("Please enter your numbers again. There seems to be a typo.");
             }
@@ -55,23 +62,23 @@ while (true) {
                 break;
             }
         }
-            return [numberOne, numberTwo];
-        }
-
-
-        var sumArray = input();
-        var sum = add(sumArray[0], sumArray[1]);
-        printResult(sum);
-        userRepeat = prompt("Would you like to enter two numbers again? Type yes or no.");
-        while (userRepeat != "no" && userRepeat != "yes") {
-            userRepeat = prompt("There seems to be a typo. Enter your response again.");
-        }
-        if (userRepeat == "no") {
-            alert("Thank you for using this program.");
-            break;
-        }
-
+        return [numberOne, numberTwo];
     }
+
+    // Displays sum, prompts user to try again if they'd like.  
+    var sumArray = input();
+    var sum = add(sumArray[0], sumArray[1]);
+    printResult(sum);
+    userRepeat = prompt("Would you like to enter two numbers again? Type yes or no.");
+    while (userRepeat != "no" && userRepeat != "yes") {
+        userRepeat = prompt("There seems to be a typo. Enter your response again.");
+    }
+    if (userRepeat == "no") {
+        alert("Thank you for using this program.");
+        break;
+    }
+
+}
 
 
 
